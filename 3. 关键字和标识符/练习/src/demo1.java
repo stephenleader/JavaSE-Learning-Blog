882bbf334c45bc383f53dadefea88dec8226bf03
stephenleader <3203548861@qq.com>
2022-05-07 16:16:49 +0800

    third

---
+++ 3. 关键字和标识符/练习/src/demo1.java
@@ -0,0 +1,8 @@
+public class demo1 {
+    public static void main(String[] args){
+        String hello = "qinjiang";
+        String $hello = "qinjiang";
+        String hello = ""
+        System.out.println(hello+$hello);
+    }
+}
---
+++ 4. 包机制/baike/src/com/kuang/operator/new1.java
@@ -0,0 +1,17 @@
+package com.kuang.operator;
+//注意观察包地址
+import java.util.Date;
+import com.kuang.newmall;
+/**注意包名要倒着写*/
+
+/**
+ *
+ */
+public class new1 {
+    public static void main(String[] args){
+
+        System.out.println("look");
+
+    }
+}
+//32/80
